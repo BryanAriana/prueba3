c8237cf6598d98271f82779b1c64844014d49022
BryanAriana <98106320+BryanAriana@users.noreply.github.com>
2022-05-17 11:20:50 -0600

    Add files via upload

---
+++ ProyectoFinalE6/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "usuario.h"
+#include "informe.h"
+using namespace std;
+
+int main()
+{
+    usuario empleado;
+    empleado.menu();
+    usuario informe;
+    return 0;
+}
